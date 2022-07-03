f9d9c11c19d8c7a1e1e49b4002b9df9ee15d0416
NohMW <jeongas9810@naver.com>
2022-07-04 06:19:56 +0900

    Modify main page

---
+++ stylesheet/main_pagination.js
@@ -0,0 +1,28 @@
+const pageMax = 3; //notice 최대 page
+var idx = 1;    //현재 page 번호
+var page = 1;   //시작 page 번호
+
+function notice_page(currentPage){
+    if(currentPage == 0){
+        if(idx==1)  return;
+        else idx-=1;
+    }
+    else if(currentPage == 4){
+        if(idx==3)  return;
+        else idx+=1;
+    }
+    else{
+        idx = currentPage;
+    }
+    $('.page-item').attr('class','page-item');
+    $('#main_page_item_'+idx).attr('class','page-item active'); 
+    if(idx == 1){
+        $('#main_page_item_prev').attr('class','page-item disabled');
+    }
+    else if(idx == 3){
+        $('#main_page_item_next').attr('class','page-item disabled');
+    }
+}
+
+$(document).ready(function(){
+});
