c1daa8d1c56a7c878ec747e0baadf3d8159ae60c
NohMW <jeongas9810@naver.com>
2022-07-12 07:24:20 +0900

    cc

--- stylesheet/ajax.js
+++ stylesheet/ajax.js
@@ -186,13 +186,10 @@ $(document).ready(function(){
          var signup_pw = $('#signup_pw').val();
          var signup_email = $('#signup_email').val();
          var signup_tel = $('#signup_tel').val();
-         var idValid = new RegExp(/^([a-z0-9]{5,})$/);
-         var pwValid = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
-         var emailValid = new RegExp(/^0-9a-zA-Z@0-9a-zA-Z\\.[a-zA-Z]{2,3}$/);
-         var phoneValid = new RegExp(/^\\d{2,3}[-]\\d{3,4}[-]\\d{4}$/);
-
-         var mystring = "heh11@@";
-         console.log("엥" + /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(mystring));
+         var idValid = new RegExp(/^[0-9|a-z|A-Z]{4,15}$/);
+         var pwValid = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/);
+         var emailValid = new RegExp(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/);
+         var phoneValid = new RegExp(/^\d{2,3}[-]\d{3,4}[-]\d{4}$/);
          
          console.log(pwValid);
          console.log(signup_pw);
