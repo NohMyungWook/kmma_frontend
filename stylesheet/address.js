cbe4ae5575a803e600f3046f2c27b5b675481eef
HYERINI <rinlee2001@naver.com>
2022-07-12 00:45:44 +0900

    edit signup

--- stylesheet/address.js
+++ stylesheet/address.js
@@ -3,12 +3,12 @@ function openDaumPostcode() {
         oncomplete: function(data) {
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
             // 우편번호와 주소 정보를 해당 필드에 넣고, 커서를 상세주소 필드로 이동한다.
-document.getElementById('PROFILE_TITLE').value = data.postcode1+data.postcode2+' '+data.address
+document.getElementById('signup_address').value = data.postcode1+data.postcode2+' '+data.address
             //전체 주소에서 연결 번지 및 ()로 묶여 있는 부가정보를 제거하고자 할 경우,
             //아래와 같은 정규식을 사용해도 된다. 정규식은 개발자의 목적에 맞게 수정해서 사용가능
             //var addr = data.address.replace(/(\s|^)\(.+\)$|\S+~\S+/g, '');
             //document.getElementById('addr').value = addr;
-            document.getElementById('PROFILE_TITLE').focus();
+            document.getElementById('signup_address').focus();
         }
     }).open();
 }
