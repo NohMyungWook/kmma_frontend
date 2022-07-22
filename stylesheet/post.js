a28dec8781019d3f366982e3403d2bc18e4277a2
HYERINI <rinlee2001@naver.com>
2022-07-23 05:37:10 +0900

    post.js

--- stylesheet/post.js
+++ stylesheet/post.js
@@ -27,15 +27,17 @@ $(document).ready(function(){
 
 //notice.html
 function getNotice(num){
+    for(var i = 0; i < 10; i++){
+        $('#notice_link' + i).removeAttr('style');
+    }
     $.ajax({
         type: "GET",
         url: domain + "noticelist?page=" + num,
         contentType: 'application/json',
         success: function(data){
             var totalPage = data.totalElements;
-            var startNum = 10 * (num - 1);
-            var pageNum = totalPage - startNum;
-            var endNum = startNum + pageNum;
+            var startNum = 0;
+            var pageNum = totalPage - 10*(num-1);
 
             if(pageNum > 10){ 
                 for(startNum; startNum < 10; startNum++){
@@ -43,13 +45,12 @@ function getNotice(num){
                     $('#notice_list_time' + startNum).html(data.content[startNum]['regdate']);
                 }
             } else{
-                for(startNum; startNum < endNum ; startNum++){
-                    $('#notice_list_title' + (startNum - 10)).html(data.content[startNum]['title']);
-                    $('#notice_list_time' + (startNum - 10)).html(data.content[startNum]['regdate']);
+                for(startNum; startNum < pageNum; startNum++){
+                    $('#notice_list_title' + startNum).html(data.content[startNum]['title']);
+                    $('#notice_list_time' + startNum).html(data.content[startNum]['regdate']);
                 }
-                for(;endNum < (10 * num); endNum++){
-                    $('#notice_list_title' + (endNum - 10)).css('display', 'none');
-                    $('#notice_list_time' + (endNum - 10)).css('display', 'none');
+                for(pageNum; pageNum < 10; pageNum++){
+                    $('#notice_link'+ pageNum).css('display', 'none');
                 }
             }
         }
@@ -63,14 +64,17 @@ $(document).ready(function(){
 
 //articleNews.html에 가져오기
 function getArticle(num){
+    for(var i = 0; i < 10; i++){
+        $('#article_link' + i).removeAttr('style');
+    }
     $.ajax({
         type: "GET",
         url: domain + "articlelist?page=" + num,
         contentType: 'application/json',
         success: function(data){
             var totalPage = data.totalElements;
-            var startNum = 5 * (num - 1);
-            var pageNum = totalPage - startNum;
+            var startNum = 0;
+            var pageNum = totalPage - 5*(num-1);
             var endNum = startNum + pageNum;
 
             if(pageNum > 5){ 
@@ -80,15 +84,13 @@ function getArticle(num){
                     $('#news_list_content' + startNum).html(data.content[startNum]['content']);
                 }
             } else{
-                for(startNum; startNum < endNum ; startNum++){
-                    $('#news_list_title' + (startNum - 5)).html(data.content[startNum]['title']);
-                    $('#news_list_time' + (startNum - 5)).html(data.content[startNum]['regdate']);
-                    $('#news_list_content' + (startNum - 5)).html(data.content[startNum]['content']);
+                for(startNum; startNum < endNum; startNum++){
+                    $('#news_list_title' + startNum).html(data.content[startNum]['title']);
+                    $('#news_list_time' + startNum).html(data.content[startNum]['regdate']);
+                    $('#news_list_content' + startNum).html(data.content[startNum]['content']);
                 }
-                for(endNum;endNum < (5 * num); endNum++){
-                    $('#news_list_title' + (endNum - 5)).css('display', 'none');
-                    $('#news_list_time' + (endNum - 5)).css('display', 'none');
-                    $('#news_list_content' + (endNum - 5)).css('display', 'none');
+                for(endNum; endNum < 5; startNum++){
+                    $('#article_link'+ endNum).css('display', 'none');
                 }
             }
         }
@@ -123,41 +125,6 @@ function getArticleContent(){
     })
 }
 
-// var data = {"content":
-// [{"articleNo":13,
-// "title":"한국메타버스미디어협회의 활동 소식을 전해드려요 13",
-// "content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..",
-// "regdate":"2022-07-22"},
-// {"articleNo":12,
-//     "title":"한국메타버스미디어협회의 활동 소식을 전해드려요 12",
-// "content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..",
-// "regdate":"2022-07-22"},
-// {"articleNo":11,
-// "title":"한국메타버스미디어협회의 활동 소식을 전해드려요 11",
-// "content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..",
-// "regdate":"2022-07-22"},
-// {"articleNo":10,
-// "title":"한국메타버스미디어협회의 활동 소식을 전해드려요 10",
-// "content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..",
-// "regdate":"2022-07-22"},
-// {"articleNo":9,"title":"한국메타버스미디어협회의 활동 소식을 전해드려요 9","content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..","regdate":"2022-07-22"},
-// {"articleNo":8,"title":"한국메타버스미디어협회의 활동 소식을 전해드려요 8","content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..","regdate":"2022-07-22"},
-// {"articleNo":7,"title":"한국메타버스미디어협회의 활동 소식을 전해드려요 7","content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..","regdate":"2022-07-22"},
-// {"articleNo":6,"title":"한국메타버스미디어협회의 활동 소식을 전해드려요 6","content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..","regdate":"2022-07-22"},
-// {"articleNo":5,"title":"한국메타버스미디어협회의 활동 소식을 전해드려요 5","content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..","regdate":"2022-07-22"},
-// {"articleNo":4,"title":"한국메타버스미디어협회의 활동 소식을 전해드려요 4","content":"한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..\r\n 한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다.. \r\n한국메타버스미디어협회는 항상 노력합니다.. 한국메타버스미디어협회는 항상 노력합니다..","regdate":"2022-07-22"}],
-// "pageable":{"sort":{"empty":true,"unsorted":true,"sorted":false},"offset":0,"pageNumber":0,"pageSize":10,"paged":true,"unpaged":false},
-// "totalPages":2,
-// "totalElements":13,
-// "last":false,
-// "size":10,
-// "number":0,
-// "sort":{"empty":true,
-//         "unsorted":true,
-//         "sorted":false},
-//         "numberOfElements":10,
-//         "first":true,
-//         "empty":false}
 
 // alert(data.content[0]['articleNo']);
 
